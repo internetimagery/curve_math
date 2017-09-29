2d4b4a40cb45e1db6bc62ac0b51d29171e61dfc2
Jason Dixon <jdixon@wetafx.co.nz>
2017-09-29 16:39:58 +1300

    adding sample code

--- logic.py
+++ logic.py
@@ -1,3 +1,4 @@
+import collections
 
 def get_attr():
     """ Get selected attributes, Return an iterable"""
@@ -7,3 +8,13 @@ def get_attr():
         if attrs:
             return set((a, cmds.attributeQuery(b, n=a, ln=True)) for a in objs for b in attrs if cmds.attributeQuery(b, n=a, ex=True))
     return []
+
+def sample_at_range(in_frame, out_frame, attributes):
+    """ Sample attributes at each frame """
+    keys = collections.defaultdict(list)
+    curr_time = cmds.currentTime(q=True)
+    for frame in range(in_frame, out_frame):
+        for attr in attributes:
+            keys[attr].append((frame, cmds.getAttr(attr)))
+    cmds.currentTime(curr_time)
+    return keys
