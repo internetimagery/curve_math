7612fedfb0785a8738c7afdccbdbbe97f5b5bb97
Jason Dixon <jason.dixon.email@gmail.com>
2017-10-01 20:16:57 +1300

    more stuff packed into one place

--- logic.py
+++ logic.py
@@ -1,4 +1,3 @@
-import collections
 
 def get_attr():
     """ Get selected attributes, Return an iterable"""
@@ -6,35 +5,20 @@ def get_attr():
     if objs:
         attrs = cmds.channelBox("mainChannelBox", sma=True, q=True) or []
         if attrs:
-            return set((a, cmds.attributeQuery(b, n=a, ln=True)) for a in objs for b in attrs if cmds.attributeQuery(b, n=a, ex=True))
+            return set("{}.{}".format(a, cmds.attributeQuery(b, n=a, ln=True)) for a in objs for b in attrs if cmds.attributeQuery(b, n=a, ex=True))
     return []
 
-def run_range(in_frame, out_frame):
-    curr_time = cmds.currentTime(q=True)
-    for frame in range(in_frame, out_frame):
-        cmds.currentTime(frame)
-        yield frame
-    cmds.currentTime(curr_time)
-
-def sample_at_range(in_frame, out_frame, attributes):
-    """ Sample attributes at each frame """
-    keys = collections.defaultdict(list)
-    for frame in run_range(in_frame. out_frame):
-        for attr in attributes:
-            keys[attr].append((frame, cmds.getAttr(attr)))
-    return keys
-
-def apply_at_range(in_frame, out_frame, attributes):
-    """ Apply keyframes at each frame """
-    for frame in run_range(in_frame, out_frame):
-        for attr in attributes:
-            for key in attributes[attr]:
-                cmds.setAttr(attr, key[1])
-
-def apply_operation(attr1, attr2, func):
-    """ apply operation to attributes """
-    new_attr = collections.defaultdict(list)
-    for at1, at2 in zip(attr1, attr2):
-        for k1, k2 in zip(at1, at2):
-            new_attr[at2].append(k2, func(k1[1], k2[1]))
-    return new_attr
+def apply_operation(in_frame, out_frame, attr1, attr2, out_attr, func):
+    err = cmds.undoInfo(openChunk=True)
+    try:
+        for frame in range(in_frame, out_frame):
+            cmds.setAttr(out_attr, func(
+                cmds.getAttr(attr1),
+                cmds.getAttr(attr2)))
+    except Exception as err:
+        pass
+    finally:
+        cmds.select(clear=True)
+        cmds.undoInfo(closeChunk=True)
+        if err:
+            cmds.undo()
