4171a17da833704507b9c820f42215cfd55d270a
Jason Dixon <jdixon@wetafx.co.nz>
2017-10-04 14:20:13 +1300

    moved logic into the one file

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 # Display a nice gui!
 import maya.cmds as cmds
-import logic
 import operator
 
 operations = {
@@ -9,6 +8,38 @@ operations = {
     "x": operator.mul,
     "/": operator.truediv
     }
+
+def get_attr():
+    """ Get selected attributes, Return an iterable"""
+    objs = cmds.ls(sl=True) or []
+    if objs:
+        attrs = cmds.channelBox("mainChannelBox", sma=True, q=True) or []
+        if attrs:
+            return set("{}.{}".format(a, cmds.attributeQuery(b, n=a, ln=True)) for a in objs for b in attrs if cmds.attributeQuery(b, n=a, ex=True))
+    return []
+
+def apply_operation(in_frame, out_frame, attr1, attr2, out_attr, func):
+    autokey = cmds.autoKeyframe(state=True, q=True)
+    err = cmds.undoInfo(openChunk=True)
+    try:
+        cmds.autoKeyframe(state=False)
+        # Collect data:
+        attr1_data = []
+        attr2_data = []
+        for frame in range(int(in_frame), int(out_frame) + 1):
+            attr1_data.append(cmds.getAttr(attr1, t=frame))
+            attr2_data.append(cmds.getAttr(attr2, t=frame))
+        for i, (at1, at2) in enumerate(zip(attr1_data, attr2_data)):
+            frame = i + in_frame
+            ans = func(at1, at2)
+            cmds.setKeyframe(out_attr, t=frame, v=ans)
+    except Exception as err:
+        raise
+    finally:
+        cmds.autoKeyframe(state=autokey)
+        cmds.undoInfo(closeChunk=True)
+        if err:
+            cmds.undo()
 
 class Main(object):
     def __init__(s):
@@ -24,7 +55,7 @@ class Main(object):
         cmds.showWindow()
 
     def select_CB(s, gui):
-        for attr in logic.get_attr():
+        for attr in get_attr():
             return cmds.textFieldButtonGrp(gui, e=True, tx=attr)
 
     def calculate(s, *_):
@@ -43,7 +74,7 @@ class Main(object):
         end = cmds.playbackOptions(q=True, max=True)
 
         # Do it!
-        logic.apply_operation(start, end, input1, input2, output, op)
+        apply_operation(start, end, input1, input2, output, op)
 
 
 if __name__ == '__main__':
